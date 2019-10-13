ca6416b12aaa2cca6f92203430c9686710e1e022
Shadow Last <gustavobessa@bemol.com.br>
2019-10-13 17:19:39 -0400

    adicionando arquivos iniciais

---
+++ casting.py
@@ -0,0 +1,6 @@
+#casting (conversao)
+idade = 24
+ano_atual = "2016"
+ano_atual = int(ano_atual)
+ano_nascimento = ano_atual - idade
+print(ano_nascimento)
---
+++ readWrite.py
@@ -0,0 +1,6 @@
+print("Hello World!")
+
+nome = input("Qual seu nome? ")
+idade = int(input("Digite aqui a sua idade: "))
+
+print("Bem vindo,",nome,",tenho", idade, "anos.")
