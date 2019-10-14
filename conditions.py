7d8146e0a381fa5ce14891e6f08a0fa34e03b59e
Shadow Last <gustavobessa@bemol.com.br>
2019-10-13 20:03:03 -0400

    13/10-20:03

---
+++ conditions.py
@@ -0,0 +1,7 @@
+idade = int(input("Qual sua idade? "))
+if (idade >= 18):
+	print("Maior de Idade")
+elif(idade < 0):
+	print("Idade Invalida")
+else:
+	print("Menor de Idade")
---
+++ repetitions.py
@@ -0,0 +1,9 @@
+print("===========")
+x = 1
+while x <=5:
+	print(x)
+	x = x + 1
+print("===========")
+for i in range(1, 6):
+	print(i)
+print("===========")
